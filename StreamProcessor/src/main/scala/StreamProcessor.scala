1b8bbff95cc60fb84d3172adcc27e699a7e489b6
Steven Fuller <steven.fuller96@gmail.com>
2023-11-17 12:46:22 -0600

    StreamProcessor scala file added

--- StreamProcessor/src/main/scala/StreamProcessor.scala
+++ StreamProcessor/src/main/scala/StreamProcessor.scala
@@ -57,17 +57,62 @@ object StreamProcessor {
             .load()
 
         //explode the data from avro
-
+        val expandedDF = inputDF
+            .withColumn("avroData", from_avro(col("value"),tradeSchema))
+            .select($"avroData.*")
+            .select(explode($"data"), $"type")
+            .select($"col.*")
 
         //rename columns and add proper timestamps
+        val finalDF = expandedDF
+            .withColumn("uuid", makeUUID())
+            .withColumnRenamed("c", "trade_conditions")
+            .withColumnRenamed("p", "price")
+            .withColumnRenamed("s", "symbol")
+            .withColumnRenamed("t", "trade_timestamp")
+            .withColumnRenamed("v", "volume")
+            .withColumn("trade_timestamp",(col("trade_timestamp")/1000).cast("timestamp"))
+            .withColumn("ingest_timestamp", current_timestamp().as("ingest_timestamp"))
 
         //write query to Cassandra
+        val query = finalDF
+            .writeStream
+            .foreachBatch { (batchDF:DataFrame,batchID:Long) =>
+                println(s"Writing to Cassandra $batchID")
+                batchDF.write
+                    .cassandraFormat(settings.cassandra("trades"),settings.cassandra("keyspace"))
+                    .mode("append")
+                    .save()
+            }
+            .outputMode("update")
+            .start()
 
         //another datafram with aggregates, running averages from last 15 schemas
+        val summaryDF = finalDF
+            .withColumn("price_volume_multiply", col("price")*col("volume"))
+            .withWatermark("trade_timestamp", "15 seconds")
+            .groupBy("symbol")
+            .agg(avg("price_volume_multiply"))
 
         //rename columns in data fram and add UUIDs before inserting to Cassandra
+        val finalsummaryDF = summaryDF
+            .withColumn("uuid", makeUUID())
+            .withColumn("ingest_timestamp", current_timestamp().as("ingest_timestamp"))
+            .withColumnRenamed("avg(price_volume_multiply)", "price_volume_multiply")
 
         //write second query to Cassandra
+        val query2 = finalsummaryDF
+            .writeStream
+            .trigger(Trigger.ProcessingTime("5 seconds"))
+            .foreachBatch{ (batchDF:DataFrame, batchID:Long) =>
+                println(s"Writing to Cassandra $batchID")
+                batchDF.write
+                    .cassandraFormat(settings.cassandra("aggregates"), settings.cassandra("keyspace"))
+                    .mode("append")
+                    .save()
+            }
+            .outputMode("update")
+            start()
 
         //let query await termination
         spark.streams.awaitAnyTermination()
